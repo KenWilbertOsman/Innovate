fc6b8419bb68c9e53f3fd6c7ab4a261c4f9fe694
Xue Qi Lee <xueqi0731@gmail.com>
2023-02-10 19:10:16 +0800

    Add information icon at the top right corner of the digital assets item

--- pages/database.js
+++ pages/database.js
@@ -2,7 +2,7 @@
 //"Creator Dashboard" Page
 
 
-import {ethers} from 'ethers'
+import { ethers } from 'ethers'
 import { useEffect, useState } from 'react'
 import axios from 'axios'
 import Web3Modal from "web3modal"
@@ -20,9 +20,9 @@ export default function Database() {
     const [nfts, setNfts] = useState([])
     const [filtered, setFiltered] = useState([])
     const [loadingState, setLoadingState] = useState('not-loaded')
-    
+
     //this is to call the loadnft once when the page is loaded
-    useEffect (() => {
+    useEffect(() => {
         loadNFTs();
         filterNFT("all");
     }, [])
@@ -33,7 +33,7 @@ export default function Database() {
         const connection = await web3Modal.connect()
         const provider = new ethers.providers.Web3Provider(connection)
         const signer = provider.getSigner()
-        
+
 
         const marketContract = new ethers.Contract(nftmarketaddress, Market.abi, signer)
         //@see go to NFTMarket.sol
@@ -68,20 +68,20 @@ export default function Database() {
     }
 
     async function filterNFT(c) {
-        
-        if (c == "all"){
+
+        if (c == "all") {
             setFiltered(nfts)
         }
         else if (c == "transit") {
             setFiltered(nfts.filter(i => (!i.completed && i.nextWarehouse != 0)))
         }
-        else if (c =="warehouse") {
+        else if (c == "warehouse") {
             setFiltered(nfts.filter(i => (!i.completed && i.nextWarehouse == 0)))
         }
         else if (c == "completed") {
             setFiltered(nfts.filter(i => i.completed))
         }
-        
+
     }
 
     if (loadingState === 'loaded' && !nfts.length) return (
@@ -89,38 +89,41 @@ export default function Database() {
     )
     else if (loadingState === 'loaded' & !filtered.length) return (
         <div>
-            <div className = "flex flex-row space-x-[50px]" id="myBtnContainer">
-                <button className = "font-bold mt-4 bg-theme-red text-white rounded py-2 px-12 shadow-lg" onClick={() => filterNFT("all")}> Show All</button>
-                <button className = "font-bold mt-4 bg-theme-red text-white rounded py-2 px-12 shadow-lg" onClick={() => filterNFT("transit")}> In Transit</button>
-                <button className = "font-bold mt-4 bg-theme-red text-white rounded py-2 px-12 shadow-lg" onClick={() => filterNFT("warehouse")}> In a Warehouse</button>
-                <button className = "font-bold mt-4 bg-theme-red text-white rounded py-2 px-12 shadow-lg" onClick={() => filterNFT("completed")}> Completed</button>
+            <div className="flex flex-row space-x-[50px]" id="myBtnContainer">
+                <button className="font-bold mt-4 bg-theme-red text-white rounded py-2 px-12 shadow-lg" onClick={() => filterNFT("all")}> Show All</button>
+                <button className="font-bold mt-4 bg-theme-red text-white rounded py-2 px-12 shadow-lg" onClick={() => filterNFT("transit")}> In Transit</button>
+                <button className="font-bold mt-4 bg-theme-red text-white rounded py-2 px-12 shadow-lg" onClick={() => filterNFT("warehouse")}> In a Warehouse</button>
+                <button className="font-bold mt-4 bg-theme-red text-white rounded py-2 px-12 shadow-lg" onClick={() => filterNFT("completed")}> Completed</button>
             </div>
             <h1 className="py-10 px-20 text-3xl">Filtered Items are Empty</h1>
         </div>
     )
     return (
         <div>
-            <div className = "flex flex-row space-x-[50px] " id="myBtnContainer">
-                <button className = "font-bold mt-4 bg-theme-red text-white rounded py-2 px-12 shadow-lg" onClick={() => filterNFT("all")}> Show All</button>
-                <button className = "font-bold mt-4 bg-theme-red text-white rounded py-2 px-12 shadow-lg" onClick={() => filterNFT("transit")}> In Transit</button>
-                <button className = "font-bold mt-4 bg-theme-red text-white rounded py-2 px-12 shadow-lg" onClick={() => filterNFT("warehouse")}> In a Warehouse</button>
-                <button className = "font-bold mt-4 bg-theme-red text-white rounded py-2 px-12 shadow-lg" onClick={() => filterNFT("completed")}> Completed</button>
+            <div className="flex flex-row space-x-[50px] " id="myBtnContainer">
+                <button className="font-bold mt-4 bg-theme-red text-white rounded py-2 px-12 shadow-lg" onClick={() => filterNFT("all")}> Show All</button>
+                <button className="font-bold mt-4 bg-theme-red text-white rounded py-2 px-12 shadow-lg" onClick={() => filterNFT("transit")}> In Transit</button>
+                <button className="font-bold mt-4 bg-theme-red text-white rounded py-2 px-12 shadow-lg" onClick={() => filterNFT("warehouse")}> In a Warehouse</button>
+                <button className="font-bold mt-4 bg-theme-red text-white rounded py-2 px-12 shadow-lg" onClick={() => filterNFT("completed")}> Completed</button>
             </div>
-            <div className = "p-4">
-                <div className = "grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-4 gap-4 pt-4">
+            <div className="p-4">
+                <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-4 gap-4 pt-4">
                     {
-                        
-                        filtered.map((nft,i) => (
-                            <div key = {i} className = "border shadow rounded-xl overflow-hidden">
-                                <img src = {nft.image} className = "rounded" />
-                                <div className = "p-4 bg-black">
-                                    <p className = "text-xl font-bold text-white">Recipient Address: {nft.recAddress}</p>
-                                    <p className = "text-xl font-bold text-white">Recipient Mobile: {nft.recNumber}</p>
-                                    <Link
-                                        className="mr-6 text-theme-red"
-                                        href={`/detail-page?index=${nft.tokenId}`} // the data
-                                        >Details
-                                    </Link>
+
+                        filtered.map((nft, i) => (
+                            <div key={i} className="border shadow rounded-xl overflow-hidden">
+                                <a href={`/detail-page?index=${nft.tokenId}`}>
+                                    <div className='flex justify-end' >
+                                        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" class="w-8 h-8 absolute m-4 cursor-pointer">
+                                            <path fill-rule="evenodd" d="M2.25 12c0-5.385 4.365-9.75 9.75-9.75s9.75 4.365 9.75 9.75-4.365 9.75-9.75 9.75S2.25 17.385 2.25 12zm8.706-1.442c1.146-.573 2.437.463 2.126 1.706l-.709 2.836.042-.02a.75.75 0 01.67 1.34l-.04.022c-1.147.573-2.438-.463-2.127-1.706l.71-2.836-.042.02a.75.75 0 11-.671-1.34l.041-.022zM12 9a.75.75 0 100-1.5.75.75 0 000 1.5z" clip-rule="evenodd"
+                                                href={`/detail-page?index=${nft.tokenId}`} />
+                                        </svg>
+                                    </div>
+                                </a>
+                                <img src={nft.image} className="rounded" />
+                                <div className="p-4 bg-black">
+                                    <p className="text-xl font-bold text-white">Recipient Address: {nft.recAddress}</p>
+                                    <p className="text-xl font-bold text-white">Recipient Mobile: {nft.recNumber}</p>
                                 </div>
                             </div>
                         ))
--- pages/my-assets.js
+++ pages/my-assets.js
@@ -3,12 +3,12 @@
 
 
 
-import {ethers} from 'ethers'
+import { ethers } from 'ethers'
 // for useState and useEffect, see this https://medium.com/recraftrelic/usestate-and-useeffect-explained-cdb5dc252baf
 import { useEffect, useState } from 'react'
 import axios from 'axios'
 import Web3Modal from 'web3modal'
-import {useRouter} from 'next/router'
+import { useRouter } from 'next/router'
 //import {BrowserRouter, Routes, Route, Navigate, Link} from 'next/router'
 import Link from 'next/link'
 
@@ -25,8 +25,8 @@ export default function MyAssets() {
     const [loadingState, setLoadingState] = useState('not-loaded')
     const router = useRouter()
     const [formInput, updateFormInput] = useState('')
-    
-    useEffect (() => {
+
+    useEffect(() => {
         loadNFTs()
     }, [])
 
@@ -35,7 +35,7 @@ export default function MyAssets() {
         const connection = await web3Modal.connect()
         const provider = new ethers.providers.Web3Provider(connection)
         const signer = provider.getSigner()
-        
+
 
         const marketContract = new ethers.Contract(nftmarketaddress, Market.abi, signer)
         const data = await marketContract.fetchMyNFTs()
@@ -56,7 +56,7 @@ export default function MyAssets() {
                 date: meta.data.date,
                 owners: i.warehouses,
                 mass: meta.data.mass
-                
+
             }
             return item
         }))
@@ -77,7 +77,7 @@ export default function MyAssets() {
         // const tokenContract = new ethers.Contract(nftaddress, NFT.abi, signer)
 
         const transaction = await marketContract.burnToken(tokenId)
-        
+
         await transaction.wait()
         router.push('/my-assets')
         loadNFTs()
@@ -89,16 +89,16 @@ export default function MyAssets() {
         const web3modal = new Web3Modal()
         const connection = await web3modal.connect()
         const provider = new ethers.providers.Web3Provider(connection)
-    
+
         const signer = provider.getSigner()
         const contract = new ethers.Contract(nftmarketaddress, Market.abi, signer)
         const prices = ethers.utils.parseUnits(nft.price.toString(), 'ether')
-        
+
         const transaction = await contract.createRequest(nft.tokenId, address, {
-          value: prices
+            value: prices
         })
-        
-    
+
+
         await transaction.wait()
         loadNFTs()
     }
@@ -108,55 +108,46 @@ export default function MyAssets() {
         <h1 className="py-10 px-20 text-3xl">No assets owned</h1>
     )
     return (
-        <div className= "flex justify-center">
-            <div className = "p-4">
-                <div className = "grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-4 gap-4 pt-4">
+        <div className="flex justify-center">
+            <div className="p-4">
+                <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-4 gap-4 pt-4">
                     {
-                        nfts.map((nft,i) => (
-                            <div key = {i} className = "border shadow rounded-xl overflow-hidden">
-                                
-                                <div className = "p-4 pg-black flex justify-end">    
-                                    <button position = "absolute" className= "w-full bg-pink-500 text-white font-bold py-1 px-7 rounded" onClick={() => burnNft(nft.tokenId)}>Burn</button>
+                        nfts.map((nft, i) => (
+                            <div key={i} className="border shadow rounded-xl overflow-hidden">
+                                <div className="p-4 pg-black flex justify-end">
+                                    <button position="absolute" className="w-full bg-pink-500 text-white font-bold py-1 px-7 rounded" onClick={() => burnNft(nft.tokenId)}>Burn</button>
                                 </div>
-
-                                <img src = {nft.image} className = "rounded" />
-                                
-                                <div className = "p-4 bg-black">
-                                    <p className = "text-xs font-bold text-white">Username: {nft.name}</p>
-                                    <p className = "text-xs font-bold text-white">Created on {nft.date}</p>
-                                    <p className = "text-xs font-bold text-white">Owners: </p>
+                                <img src={nft.image} className="rounded" />
+                                <div className="p-4 bg-black">
+                                    <p className="text-xs font-bold text-white">Username: {nft.name}</p>
+                                    <p className="text-xs font-bold text-white">Created on {nft.date}</p>
+                                    <p className="text-xs font-bold text-white">Owners: </p>
                                     {
                                         nft.owners.map((owner, j) => (
-                                                (<p key = {j} className = "text-xs font-bold text-white">- {owner}</p>)
+                                            (<p key={j} className="text-xs font-bold text-white">- {owner}</p>)
                                         )
                                         )
                                     }
-                                    
                                 </div>
-                                <div className = "flex justify-center">
-
-                                <br></br><select 
-                                    id="large" 
-                                    onChange={e => updateFormInput(e.target.value)}
-                                    className="block w-full px-4 py-3 text-base text-gray-900 border border-gray-300 rounded-lg bg-gray-50 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500">                       
-                                        <option value = '' selected>Warehouse to be Sent</option>
+                                <div className="flex justify-center">
+                                    <br></br><select
+                                        id="large"
+                                        onChange={e => updateFormInput(e.target.value)}
+                                        className="block w-full px-4 py-3 text-base text-gray-900 border border-gray-300 rounded-lg bg-gray-50 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500">
+                                        <option value='' selected>Warehouse to be Sent</option>
                                         <option value='0xf39Fd6e51aad88F6F4ce6aB8827279cffFb92266'>Account 3</option>
-                                </select>
-                                    <div className = "p-4 pg-black flex justify-end">    
-                                        <button className= "w-quarter bg-pink-500 text-white font-bold py-1 px-4 rounded" onClick={() => requestNFT(nft)}>Request</button>
+                                    </select>
+                                    <div className="p-4 pg-black flex justify-end">
+                                        <button className="w-quarter bg-pink-500 text-white font-bold py-1 px-4 rounded" onClick={() => requestNFT(nft)}>Request</button>
                                         <Link
-                                        href={`/detail-page?index=${nft.tokenId}`} // the data
+                                            href={`/detail-page?index=${nft.tokenId}`} // the data
                                         >Details
                                         </Link>
                                     </div>
                                 </div>
-                                
-                             </div>
-                            
-                        )
-                        )
+                            </div>
+                        ))
                     }
-
                 </div>
             </div>
         </div>
