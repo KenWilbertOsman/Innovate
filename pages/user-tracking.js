48e17cc3e946ddf2890a7f831c4229d294cc1560
KenWilbertOsman <87554577+KenWilbertOsman@users.noreply.github.com>
2023-02-17 15:50:46 +0800

    -color changed a bit - navbar is responsive for phone and desktop

--- pages/user-tracking.js
+++ pages/user-tracking.js
@@ -27,7 +27,9 @@ export default function UserTracking() {
         </div>
         
       </div>
-      <footer class="p-4 bg-white sm:p-6 dark:bg-gray-900">
+      <div class = "flex flex-col min-h-screen">
+      <footer class="p-4 bg-theme-beige sm:p-6 mt-auto">
+          
     <div class="mb:justify-evenly">
         
         <div class="grid grid-cols-2 gap-8 sm:gap-6 sm:grid-cols-3">
@@ -57,18 +59,18 @@ export default function UserTracking() {
                 <h2 class="mb-6 text-xl font-semibold text-gray-900 uppercase dark:text-white">Legal</h2>
                 <ul class="text-gray-600 dark:text-gray-400">
                     <li class="mb-4">
-                        <a href="#" class="hover:underline">Privacy Policy</a>
+                        <a href="/user-tracking" class="hover:underline">Privacy Policy</a>
                     </li>
                     <li>
-                        <a href="#" class="hover:underline">Terms &amp; Conditions</a>
+                        <a href="/user-tracking" class="hover:underline">Terms &amp; Conditions</a>
                     </li>
                 </ul>
             </div>
         </div>
     </div>
-    <hr class="my-6 border-gray-200 sm:mx-auto dark:border-gray-700 lg:my-8" />
+    <hr class="my-6 border-black sm:mx-auto dark:border-gray-700 lg:my-8" />
     <div class="sm:flex sm:items-center sm:justify-between">
-        <span class="text-sm text-gray-500 sm:text-center dark:text-gray-400">© 2023 <a href="https://flowbite.com/" class="hover:underline">Flowbite™</a>. All Rights Reserved.
+        <span class="text-sm text-gray-500 sm:text-center dark:text-gray-400">© 2023 <a href="https://flowbite.com/" class="hover:underline">DreamCatcher™</a>. All Rights Reserved.
         </span>
         <div class="flex mt-4 space-x-6 sm:justify-center sm:mt-0">
             <a href="#" class="text-gray-500 hover:text-gray-900 dark:hover:text-white">
@@ -94,6 +96,7 @@ export default function UserTracking() {
         </div>
     </div>
 </footer>
+  </div>
   </div>
     )
   }
