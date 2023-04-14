b73af52d070df1ee17b6fbcf7463baf4a312ad20
Xue Qi Lee <xueqi0731@gmail.com>
2023-04-15 01:26:11 +0800

    change gradient theme

--- components/ShopNavbar.js
+++ components/ShopNavbar.js
@@ -15,10 +15,10 @@ function ShopNavbar() {
     
      <div><p className="flex flex-wrap "><span className="text-5xl flex flex-wrap text-theme-dream font-serif font-normal tracking-wider">DREAM</span><span className="text-5xl font-serif font-semibold tracking-wider flex flex-wrap">CATCHER</span></p></div>
     <div className="flex flex-wrap mt-4">     {/**This div to hold link, mt-4 = Margin top 4 */}
-        <Link href="/" className="mr-4 text-theme-peach">
+        <Link href="/" className="mr-4 text-theme-dream-2">
             Home
         </Link>
-        <Link href="/create-item" className="mr-4 text-theme-peach">
+        <Link href="/create-item" className="mr-4 text-theme-dream-2">
             Create Parcel
         </Link>
 
