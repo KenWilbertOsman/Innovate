7c10eb768aa655907370c3038b0a6ae9683d4182
Xue Qi Lee <xueqi0731@gmail.com>
2023-04-17 23:53:22 +0800

    modified design of submit button, make it responsive

--- components/ShopNavbar.js
+++ components/ShopNavbar.js
@@ -13,7 +13,6 @@ function ShopNavbar() {
     const router = useRouter()
     return (
         <nav className="p-7 bg-gradient-to-r from-slate-100 to-theme-blue ">   {/*USING TAILWIND:  Border bottom and padding 6*/}
-
             <div><p className="flex flex-wrap "><span className="text-5xl flex flex-wrap text-theme-dream font-serif font-normal tracking-wider">DREAM</span><span className="text-5xl font-serif font-semibold tracking-wider flex flex-wrap">CATCHER</span></p></div>
             <div className="flex flex-wrap mt-4">     {/**This div to hold link, mt-4 = Margin top 4 */}
                 <Link href="/" className="mr-4 text-theme-dream-2">
@@ -22,20 +21,16 @@ function ShopNavbar() {
                 <Link href="/create-item" className="mr-4 text-theme-dream-2">
                     Create Parcel
                 </Link>
-
-                <div class="relative flex items-center lg:ml-auto md:ml-auto">
-                    <label for="warehouse" class="text-theme-dream-2">Default Warehouse:</label>
-                    <select id="warehouse" class="block bg-white border border-gray-300 text-gray-700 pl-2 pr-2 rounded-md leading-tight focus:outline-none focus:bg-white focus:border-gray-500">
+                <div class="relative flex items-center mt-4 lg:mt-0 md:mt-0 lg:ml-auto md:ml-auto">
+                    <label for="warehouse" class="text-theme-dream-2 mr-1">Default Warehouse:</label>
+                    <select id="warehouse" class="block bg-white border border-gray-300 text-gray-700 pl-2 pr-2 mr-1 rounded-md leading-tight focus:outline-none focus:bg-white focus:border-gray-500">
                         <option value="1" class="py-2">Warehouse 1</option>
                         <option value="2" class="py-2">Warehouse 2</option>
                     </select>
-                    <button class=" flex items-center px-4 text-white bg-theme-peach" type="submit">
+                    <button class=" flex items-center px-4 py-1 text-white bg-theme-peach" type="submit">
                         <i class="fas fa-check mr-1"></i>
-                        Submit
                     </button>
                 </div>
-
-
             </div>
             <span className="top-0 right-0 absolute pr-6 pt-10" onClick={() => router.push('/profile')}>
                 <HiUser size={45} color="808080" />
